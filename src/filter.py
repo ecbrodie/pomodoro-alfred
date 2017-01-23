04c69131b16895d6562cc112501f5340adb35e2b
Evan Brodie <brodie.evan@gmail.com>
2017-01-23 17:22:26 -0500

    extract item creation into its own function

--- src/filter.py
+++ src/filter.py
@@ -10,15 +10,20 @@ def main(wf):
     # Get args from Workflow, already in normalized Unicode
     args = wf.args
 
-    wf.add_item(u'pomo-start', u'Start a pomodoro')
-    wf.add_item(u'pomo-stop', u'Stop a pomodoro')
-    wf.add_item(u'pomo-break', u'Start a break')
+    for item in items():
+        wf.add_item(item['title'], item['description'], arg=item['arg'])
 
     # Send output to Alfred. You can only call this once.
     # Well, you *can* call it multiple times, but Alfred won't be listening
     # any more...
     wf.send_feedback()
 
+def items():
+    return [
+        dict(title='pomo-start', description='Start a pomodoro', arg='start'),
+        dict(title='pomo-stop', description='Stop a pomodoro', arg='stop'),
+        dict(title='pomo-break', description='Start a break', arg='break'),
+    ]
 
 if __name__ == '__main__':
     # Create a global `Workflow` object
